e2fd86e93ff3c3046c00596c625b4a8e9a25c419
Vinod <vinod@leftshift.io>
2015-12-29 15:56:25 +0530

    Initial Commit

---
+++ TabbarDemo/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  TabbarDemo
+//
+//  Created by Vinod on 29/12/15.
+//  Copyright © 2015 LeftShift Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ TabbarDemoTests/TabbarDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  TabbarDemoTests.m
+//  TabbarDemoTests
+//
+//  Created by Vinod on 29/12/15.
+//  Copyright © 2015 LeftShift Technologies. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TabbarDemoTests : XCTestCase
+
+@end
+
+@implementation TabbarDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
